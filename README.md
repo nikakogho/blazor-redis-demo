e1bec54f3a5d63794acbb89190fb82b186e2a548
nikakogho <nikakoghuashvili@gmail.com>
2021-05-25 23:04:06 +0400

    First step

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple use of [Redis](https://redis.io/topics/introduction) with [Blazor Serverside](https://medium.com/ingeniouslysimple/blazor-server-side-looking-at-single-page-apps-from-a-different-angle-356a0972dca7)
+
+### Based on [this tutorial](https://www.youtube.com/watch?v=UrQWii_kfIE) by [IAmTimCorey](https://www.youtube.com/channel/UC-ptWR16ITQyYOglXyQmpzw)
+
+Application connects to a redis server that supposedly runs on port **4200** of **localhost** (can be changed in *appsettings.json*)
---
+++ RedisDemo/Helpers/DistributedClassHelpers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Helpers
+{
+    public static class DistributedClassHelpers
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, 
+            string key, T data,
+            TimeSpan? fullExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = fullExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+
+            string json = JsonSerializer.Serialize(data);
+            
+            await cache.SetStringAsync(key, json, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string key)
+        {
+            string json = await cache.GetStringAsync(key);
+
+            if (json is null) return default;
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
